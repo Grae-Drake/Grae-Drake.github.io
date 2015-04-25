1f31aaf91bda4b74f732463348f037bb65060672
Grae Drake <graemdrake@gmail.com>
2015-04-24 22:21:07 -0400

    Add line numbers

--- js/app.js
+++ js/app.js
@@ -2,5 +2,12 @@ var myCodeMirror = CodeMirror(document.getElementById("editor"), {
   value: ["function myScript(){return 100;}",
           "var someText = 'Yo, dawg'"].join("\n"),
   mode:  {name: "javascript", json: true},
-  theme: "solarized dark"
+  theme: "solarized dark",
+  lineNumbers: true
+});
+
+$(document).ready(function (){
+  myCodeMirror.on("click", function() {
+    console.log(myCodeMirror.getDoc());
+  });
 });
