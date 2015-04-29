bf801a61932da610cc9bf0a6fab865e0a1070d50
Grae Drake <graemdrake@gmail.com>
2015-04-29 17:33:45 -0400

    Linting is a go

--- js/app.js
+++ js/app.js
@@ -2,17 +2,18 @@ var myCodeMirror = CodeMirror(document.getElementById("editor"), {
   value: ["function myScript(){return 100;}",
           "var someText = 'Yo, dawg'"].join("\n"),
   mode:  {name: "javascript", json: true},
-  theme: "solarized dark",
   lineNumbers: true,
+  gutters: ["CodeMirror-lint-markers"],
+  lint: true,
+  theme: "solarized dark",
   autoCloseBrackets: true,
-  //lint: true,
   matchBrackets: true
 });
 
 $(document).ready(function (){
 
   $(".CodeMirror").on("click", function() {
-    console.log(myCodeMirror.getDoc());
+    console.log(myCodeMirror.getValue());
   });
 
 });
