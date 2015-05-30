e0f5a8677bac72882242ddb098ab20bbc25362c7
Grae Drake <graemdrake@gmail.com>
2015-05-30 13:27:55 -0400

    Another

--- js/app.js
+++ js/app.js
@@ -20,6 +20,7 @@ $(document).ready(function (){
   $("button[name='Problem_1']").on("click", function(){
     var problemData = $.get("Python_Euler/Problem_1.py", function() {
       console.log(problemData["responseText"]);
+      myCodeMirror.setValue(problemData["responseText"]);
     });
     
   });
